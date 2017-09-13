d5bf1c053c9959e218efe2a15336bbb7bb2cc964
akswii <aksel.christoffer.wiig@gmail.com>
2017-09-13 16:26:36 +0200

    omw

--- src/oblig1/Oblig1.java
+++ src/oblig1/Oblig1.java
@@ -7,11 +7,11 @@ import java.util.*;
  * Aksel Wiig s232324
  */
 public class Oblig1 {
-    
-    public static void main(String... args){
+
+    public static void main(String... args) {
         int[] random = randPerm(10);
         System.out.println(Arrays.toString(random));
-        System.out.println(min(random));
+        System.out.println(ombyttinger(random));
         System.out.println(Arrays.toString(random));
     }
 
@@ -20,7 +20,7 @@ public class Oblig1 {
         a[i] = a[j];
         a[j] = temp;
     }
-    
+
     public static int[] randPerm(int n) { //genererer en tilfeldig array med tall
         Random r = new Random();
         int[] a = new int[n];
@@ -37,25 +37,45 @@ public class Oblig1 {
     }
 
     public static int min(int[] a) { //må finne antall sammenligninger 
-        if(a.length == 0){
-            throw new NoSuchElementException("Tabellen er tom!");
+        if (a.length == 0) {
+            throw new NoSuchElementException("Tabellen er tom!"); 
         }
         
-        for (int i = 0; i < a.length-1; i++) {
-            if(a[i] > a[i+1]){
-                bytt(a,i,i+1);
-                System.out.println(Arrays.toString(a));
+        int m = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[m] > a[i]) {
+                bytt(a,m,i);
             }
         }
         return a[0];
     }
-    
-    public static int ombyttinger(int[] a){
-        int ant = 0;
+
+    public static int ombyttinger(int[] a) { //mer eller mindre effektiv?
+        if (a.length == 0) {
+            throw new NoSuchElementException("Tabellen er tom!"); 
+        }
         
-        
-        
+        int m = 0, ant = 0;
+
+        for (int i = 1; i < a.length; i++) {
+            if (a[m] > a[i]) {
+                bytt(a,m,i);
+                ant++;
+            }
+        }
         return ant;
     }
     
+    public static int modus(int[] a){
+        if(a.length < 0){ throw new IllegalStateException("Arrayen er tom."); }
+        
+        for(int i = 0; i < a.length-1;i++){
+            if(a[i+1]>a[i]){
+                throw new IllegalStateException("Tabellen er ikke sortert.");
+            }
+        }
+        
+        return 0;   
+    }
 }
