df9dbb8787aa84c8a8620723ab235fa3ba2fe5d2
akswii <aksel.christoffer.wiig@gmail.com>
2017-09-28 14:18:50 +0200

    alt er ferdig

--- src/oblig1/Oblig1.java
+++ src/oblig1/Oblig1.java
@@ -3,28 +3,14 @@ package oblig1;
 import java.util.*;
 
 /**
- * Aksel Wiig s232324
- * Tommy Tran s315573
+ * Aksel Wiig s232324 Tommy Tran s315573
  */
 public class Oblig1 {
-
-    public static void main(String... args) {
-        int[]a = {1,2,3,4,5,6};
-        System.out.println(Arrays.toString(sumX(a, 5)));
-    }
 
     public static void bytt(int[] a, int i, int j) {
         int temp = a[i];
         a[i] = a[j];
         a[j] = temp;
-    }
-
-    public static void sortering(int[] a, int v, int h) {
-        for (int i = h; i > v; i--) {
-            int m = maks(a, v, i);
-            System.out.print(m + ",");
-            bytt(a, i - 1, m);
-        }
     }
 
     public static int antPlasser(int t) {
@@ -73,34 +59,19 @@ public class Oblig1 {
         return m;
     }
 
-    public static int[] randPerm(int n) {
-        Random r = new Random();
-        int[] a = new int[n];
-
-        Arrays.setAll(a, i -> i + 1);
-
-        for (int k = n - 1; k > 0; k--) {
-
-            int i = r.nextInt(k + 1);
-            bytt(a, k, i);
-        }
-
-        return a;
-    }
-
-    public static int min(int[]a){
+    public static int min(int[] a) {
         int n = a.length;
-        if(n < 0){
+        if (n < 0) {
             throw new NoSuchElementException("Tom array");
         }
-      
-        for (int i = n-1; i > 0; i--) {
-            if (a[i] < a[i-1]) {
-                bytt(a, i, i-1);
+
+        for (int i = n - 1; i > 0; i--) {
+            if (a[i] < a[i - 1]) {
+                bytt(a, i, i - 1);
             }
         }
         return a[0];
-        
+
     }
 
     public static int ombyttinger(int[] a) {
@@ -173,11 +144,9 @@ public class Oblig1 {
         int lengde = a.length;
         int v = 0, h = lengde - 1;
 
-        while (v < h) {
+        while (v <= h) {
             if (((a[v] % 2) == 0) && !((a[h] % 2) == 0)) {
-                bytt(a, v, h);
-                v++;
-                h--;
+                bytt(a, v++, h--);
             } else if ((a[v] % 2) == 0) {
                 h--;
             } else if (!((a[h] % 2) == 0)) {
@@ -187,7 +156,8 @@ public class Oblig1 {
                 h--;
             }
         }
-        //System.out.println(Arrays.toString(a) + " " + v + " " + h);
+        Arrays.sort(a, 0, v);
+        Arrays.sort(a, v, lengde);
     }
 
     public static void rotasjon(char[] a) {
@@ -254,7 +224,7 @@ public class Oblig1 {
 
         return r;
     }
-    
+
     //Effektivitetstesting
     public static int min(int[] a, int fra, int til) {
         fratilKontroll(a.length, fra, til);
@@ -277,8 +247,7 @@ public class Oblig1 {
     }
 
     public static void innsettingssortering(int[] a) {
-        for (int i = 1; i < a.length; i++)
-        {
+        for (int i = 1; i < a.length; i++) {
             int verdi = a[i], j = i - 1;
             for (; j >= 0 && verdi < a[j]; j--) {
                 a[j + 1] = a[j];
@@ -288,13 +257,12 @@ public class Oblig1 {
     }
 
     private static int parter0(int[] a, int v, int h, int skilleverdi) {
-        while (true)
-        {
+        while (true) {
             while (v <= h && a[v] < skilleverdi) {
                 v++;
             }
             while (v <= h && a[h] >= skilleverdi) {
-                h--;    
+                h--;
             }
             if (v < h) {
                 bytt(a, v++, h--);
@@ -305,18 +273,15 @@ public class Oblig1 {
     }
 
     public static void fratilKontroll(int tablengde, int fra, int til) {
-        if (fra < 0)
-        {
+        if (fra < 0) {
             throw new ArrayIndexOutOfBoundsException("fra(" + fra + ") er negativ!");
         }
 
-        if (til > tablengde)
-        {
+        if (til > tablengde) {
             throw new ArrayIndexOutOfBoundsException("til(" + til + ") > tablengde(" + tablengde + ")");
         }
 
-        if (fra > til)
-        {
+        if (fra > til) {
             throw new IllegalArgumentException("fra(" + fra + ") > til(" + til + ") - illegalt intervall!");
         }
 
@@ -327,40 +292,36 @@ public class Oblig1 {
 
     private static int sParter0(int[] a, int v, int h, int indeks) {
         bytt(a, indeks, h);
-        int pos = parter0(a, v, h - 1, a[h]);  
-        bytt(a, pos, h);              
-        return pos;                   
+        int pos = parter0(a, v, h - 1, a[h]);
+        bytt(a, pos, h);
+        return pos;
     }
 
-    private static void kvikksortering0(int[] a, int v, int h)
-    {
+    private static void kvikksortering0(int[] a, int v, int h) {
         if (v >= h) {
             return;
         }
         int k = sParter0(a, v, h, (v + h) / 2);
-        kvikksortering0(a, v, k - 1);     
-        kvikksortering0(a, k + 1, h);     
+        kvikksortering0(a, v, k - 1);
+        kvikksortering0(a, k + 1, h);
     }
 
-    public static void kvikksortering(int[] a, int fra, int til)
-    {
+    public static void kvikksortering(int[] a, int fra, int til) {
         fratilKontroll(a.length, fra, til);
         kvikksortering0(a, fra, til - 1);
     }
 
-    public static void kvikksortering(int[] a)
-    {
+    public static void kvikksortering(int[] a) {
         kvikksortering0(a, 0, a.length - 1);
     }
 
     private static void flett(int[] a, int[] b, int fra, int m, int til) {
         int n = m - fra;
-        System.arraycopy(a, fra, b, 0, n); 
+        System.arraycopy(a, fra, b, 0, n);
 
         int i = 0, j = m, k = fra;
 
-        while (i < n && j < til)
-        {                       
+        while (i < n && j < til) {
             a[k++] = b[i] <= a[j] ? b[i++] : a[j++];
         }
 
@@ -373,19 +334,19 @@ public class Oblig1 {
         if (til - fra <= 1) {
             return;
         }
-        int m = (fra + til) / 2;       
+        int m = (fra + til) / 2;
 
-        flettesortering(a, b, fra, m); 
-        flettesortering(a, b, m, til); 
+        flettesortering(a, b, fra, m);
+        flettesortering(a, b, m, til);
 
         if (a[m - 1] > a[m]) {
-            flett(a, b, fra, m, til);  
+            flett(a, b, fra, m, til);
         }
     }
 
     public static void flettesortering(int[] a) {
         int[] b = Arrays.copyOf(a, a.length / 2);
-        flettesortering(a, b, 0, a.length);      
+        flettesortering(a, b, 0, a.length);
     }
 
     public static void tidSorteringer(int[] a) {
@@ -415,15 +376,15 @@ public class Oblig1 {
         tid = System.currentTimeMillis() - tid;
         System.out.println("Flettesortering: " + tid);
     }
-    
-     public static int[] sumX(int[] a, int x){
-         for (int i = 0; i < 10; i++) {
-             for (int j = 0; j < 10; j++) {
-                 if((a[i] + a[j]) == x){
-                     return new int[]{a[i],a[j]};
-                 }
-             }
-         }
-         return null;
-     }
+
+    public static int[] sumX(int[] a, int x) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                if ((a[i] + a[j]) == x) {
+                    return new int[]{a[i], a[j]};
+                }
+            }
+        }
+        return null;
+    }
 }
